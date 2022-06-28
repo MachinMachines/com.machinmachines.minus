ec4a0893c69c694a91c6f2540e960025e9d487fe
Gabriel Bonnefond <gabriel.bonnefond@gmail.com>
2022-06-28 17:47:47 +0200

    add Synchro with Cortex

--- MinusWindow.cs
+++ MinusWindow.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEditor.PackageManager.Requests;
+using UnityEditor.PackageManager;
+using System.IO;
+using System;
 
 namespace StudioManette.minus
 {
@@ -23,6 +27,24 @@ namespace StudioManette.minus
 
         public void OnGUI()
         {
+            //GROUP 1 : PROPERTIES
+            EditorGUILayout.BeginVertical("Box");
+            {
+                EditorGUILayout.LabelField("PROPERTIES", EditorStyles.boldLabel);
+
+                currentMinusSettings = (MinusSettingsObject)EditorGUILayout.ObjectField(currentMinusSettings, typeof(MinusSettingsObject), false);
+            }
+            EditorGUILayout.EndVertical();
+
+            if (GUILayout.Button("Synchronize Self"))
+            {
+                Synchronize();
+            }
+
+            if (GUILayout.Button("Read Cortex Manifest"))
+            {
+                ReadCortexManifest();
+            }
         }
 
         private void Init()
@@ -41,5 +63,63 @@ namespace StudioManette.minus
                 currentMinusSettings = settingsList[0];
             }
         }
+
+
+        static ListRequest request;
+
+        private void Synchronize()
+        {
+            Debug.Log("Synchronize !! ");
+            request = Client.List();
+            EditorApplication.update += ProgressListPackage;
+        }
+
+
+        static void ProgressListPackage()
+        {
+            if (request.IsCompleted)
+            {
+                if (request.Status == StatusCode.Success)
+                {
+                    foreach (UnityEditor.PackageManager.PackageInfo pcInfo in request.Result)
+                    {
+                        Debug.Log("package info : " + pcInfo.name + " / version : " + pcInfo.version);
+                    }
+                }
+                else if (request.Status >= StatusCode.Failure)
+                {
+                    Debug.Log(request.Error.message);
+                }
+
+                EditorApplication.update -= ProgressListPackage;
+            }
+        }
+
+        void ReadCortexManifest()
+        {
+            //try
+            {
+                StreamReader reader = new StreamReader(currentMinusSettings.primaryProject.path + "/Packages/manifest.json");
+                string strManifest = reader.ReadToEnd();
+                Debug.Log("strManifest : " + strManifest);
+                Dictionary<string, string> packages =  JsonUtility.FromJson<JSONPackageManifest>(strManifest).dependencies;
+                foreach (KeyValuePair<string, string> package in packages)
+                {
+                    Debug.Log("package name : " + package.Key +  " / version : " + package.Value);
+                }
+            }
+            /*
+            catch (Exception e)
+            {
+                EditorUtility.DisplayDialog("error", "error : " + e.Message, "ok");
+            }
+            */
+        }
+    }
+
+    [System.Serializable]
+    public class JSONPackageManifest
+    {
+        public Dictionary<string, string> dependencies;
     }
 }
